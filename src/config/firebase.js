d13016e97c86bee51256c1711e664654a274d6c2
Jordan Tannenbaum <jtannenbaum3@gatech.edu>
2025-12-01 15:12:58 -0800

    Move API keys to .env and regenerated keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBdGgqYIcqpnlLc3_88qzsuInh0IZZGX-8',
-	authDomain: 'algo-recall.firebaseapp.com',
-	projectId: 'algo-recall',
-	storageBucket: 'algo-recall.firebasestorage.app',
-	messagingSenderId: '501910309076',
-	appId: '1:501910309076:web:c23e17ec5410ac0f191341',
-	measurementId: 'G-W9P6J31PB7',
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
