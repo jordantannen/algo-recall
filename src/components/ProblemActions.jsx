c2be43bff87d922b3126e7cadaac58d22b6d0cff
Jordan Tannenbaum <jtannenbaum3@gatech.edu>
2026-01-17 13:20:42 -0800

    Remove debugging Unsolved duedate

--- src/components/ProblemActions.jsx
+++ src/components/ProblemActions.jsx
@@ -13,7 +13,7 @@ export default function ProblemActions({
 				className='flex gap-2 mt-4 pt-4 border-t border-gray-100'
 			>
 				<button
-					onClick={() => onReviewDate(0)}
+					onClick={() => onReviewDate(1, true)}
 					className='flex-1 bg-red-50 hover:bg-red-100 text-red-700 py-2 rounded text-sm transition-colors cursor-pointer'
 				>
 					Unsolved
